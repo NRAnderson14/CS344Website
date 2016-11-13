6c61ab607f1143a7e050dd36315f3e2a6202ec94
nranderson14 <NRAnderson14@winona.edu>
2016-11-12 20:20:12 -0600

    Actually added stuff

---
+++ Final/index.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Winona State Computer Science</title>
+<!--
+      
+    <script src="../JS/javascript.js" type="text/javascript"></script>
+-->
+    
+    <link rel="stylesheet" href="Style/style.css"> 
+      
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900|Slabo+27px" rel="stylesheet">
+      
+  </head>
+
+  <body>
+      <header><img src="Images/winonalogo.png">COMPUTER  SCIENCE</header>
+      <nav>
+          <a href="">HOME</a>
+          <a href="">PROSPECTIVE STUDENTS</a>
+          <a href="">CURRENT STUDENTS</a>
+          <a href="">FACULTY</a>
+          <a href="">RESOURCES</a>
+      </nav>
+      <main>
+      
+          <img src="Images/Banner.jpg" alt="Banner" class="banner">
+          
+          <h1 class="mainheading">COMPUTER SCIENCE AT WINONA STATE</h1>
+          
+          
+          <p class="intropara">
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. 
+          </p>
+          
+      <br>
+          
+          <div class="wrapper">
+              <div class="wrapperin">
+                  
+                  <section class="set">
+                    <a href="" class="options"><img src="Images/Prospective.png" alt="Prospective Students">
+                    <h2 class="optionsheading">PROSPECTIVE STUDENTS</h2></a>
+                  </section>
+                  
+                  <section class="set">
+                    <a href="" class="options"><img src="Images/Current.png" alt="Current Students">
+                    <h2 class="optionsheading">CURRENT STUDENTS</h2></a>
+                  </section>
+                  
+                  <section class="set">
+                    <a href="" class="options"><img src="Images/Faculty.png" alt="Faculty and Alumni">
+                    <h2 class="optionsheading">FACULTY & ALUMNI</h2></a>
+                  </section>
+                      
+              </div>
+          </div>
+          <br>
+          <div class="eventswrapper">
+              <div class="widget">
+              <h2 class="optionsheading">
+                  UPCOMING EVENTS</h2>
+                  
+                  <div class="events">
+                  <h3> Winona State Hackathon 2016</h3>
+                  <p> Some information about this event blah blah blah blahblah</p>
+                  
+                  <h3> Some Other Spectacular Event</h3>
+                  <p>Blah blah blah blah blah blah blah blah blah blah blhaaaaahblablablablaaaahhhhh</p>
+                  
+                  <h3> And Another Event 2017</h3>
+                  <p>Blah blah blah blah blah blah blah blah blah blah blhaaaaahblablablablaaaahhhhh</p>
+                  
+                  <h3> And Another Event 2018</h3>
+                  <p>Blah blah blah blah blah blah blah blah blah blah blhaaaaahblablablablaaaahhhhh</p>
+                  
+                  <h3> And Another Event 2018</h3>
+                  <p>Blah blah blah blah blah blah blah blah blah blah blhaaaaahblablablablaaaahhhhh</p>
+                  </div>
+              </div>
+              
+              <div class="verticalline">
+              
+              </div>
+              
+              <div class="widget">
+              <h2 class="optionsheading">
+                  CURRENT RESEARCH</h2>
+                  
+                  <div class="events">
+                  <img src="Images/ResearchProject.png" alt="Research Project">
+                  
+                  <h3> Winona State Hackathon 2016</h3>
+                  <p> Some information about this event blah blah blah blahblah</p>
+                  </div>
+                  
+              </div>
+          
+          </div>
+          
+      
+      </main>
+      
+      <br>
+      
+      <footer>
+          <p>Created and Developed by Evan, Nathan, Joe and Uzma</p>
+          <img src="Images/Footer.png" alt="footer"></footer>
+   
+  </body>
+</html>
