9879c8f1b17bf3d87c3c4fe1a35b0ba9306e4830
nranderson14 <NRAnderson14@winona.edu>
2016-11-18 13:25:58 -0600

    Moved stuff around again

--- confirmation.php
+++ confirmation.php
@@ -6,7 +6,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<script src="index.js" type="text/javascript"></script>
 		<script src="jquery.js" type="text/javascript"></script>
-		<link rel="stylesheet" href="style.css">
+		<link rel="stylesheet" href="generatorstyle.css">
 
 	</head>
 		<body>
